f11f3694425f8849f07106f76bf81df79ff648e4
Anton Grudnov <stepfano@yandex.ru>
2020-10-03 22:51:15 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int var  = 90;
+        while (var > 0){
+            System.out.println(var);
+            var -=5;
+        }
+    }
+}
